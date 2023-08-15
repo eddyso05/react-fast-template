2c8e43bff3b24041806c101fbb2fe869e0da40ea
eddy05 <eddysiow0530@hotmail.com>
2023-08-15 13:42:17 +0800

    fix: get user file path

--- src/commands/addComponent.js
+++ src/commands/addComponent.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { getCurrentModuleDirectory } from "../utils/getCurrentDirectory.js";
+import { getUserModuleDirectory } from "../utils/getCurrentDirectory.js";
 import { uploadFolder } from "../services/drive.js";
 import path from "path";
 import fs from "fs";
 
 // find the current path -> bin of the project that user want to save the template
 export const addComponent = () => {
-  const sourceDirectory = path.join(getCurrentModuleDirectory(path), "bin");
+  const sourceDirectory = path.join(getUserModuleDirectory(path), "bin");
 
   if (!fs.existsSync(sourceDirectory)) {
     console.error(`Source directory '${sourceDirectory}' not found.`);
--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import { createRequire } from "module";
 export default async function main(args) {
   const localRequire = createRequire(import.meta.url);
   const pkg = localRequire("../package.json");
-
-  // init dotenv
-  // dotEnvConfig({ path: path.resolve(process.cwd(), ".env.local") });
-
-  // Initialize generate component command
-
   const program = ComponentCommand(args);
 
   program.version(pkg.version);
