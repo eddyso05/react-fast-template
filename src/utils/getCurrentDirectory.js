3741741bf5b9a6b699a2346b06f3856f74235a1b
eddy05 <eddysiow0530@hotmail.com>
2023-08-16 10:13:43 +0800

    fix: remove unused code

--- src/utils/getCurrentDirectory.js
+++ src/utils/getCurrentDirectory.js
@@ -4,12 +4,3 @@ export const getCurrentModuleDirectory = (path) => {
   const parentDirectoryPath = path.dirname(directoryPath);
   return parentDirectoryPath;
 };
-
-export const getUserModuleDirectory = (path) => {
-  // Get the directory path of the current executing script
-  const stack = new Error().stack;
-  const callerFilePath = stack.split("\n")[2].match(/\((.*):\d+:\d+\)$/)[1];
-
-  const directoryPath = path.dirname(callerFilePath);
-  return directoryPath;
-};
